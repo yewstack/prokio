f2c63e3646773f5dbf09986f71d8abda86b0a015
伊欧 <langyo.china@gmail.com>
2023-11-17 15:47:52 +0800

    Try to add WASI target support.

--- src/rt_tokio/mod.rs
+++ src/rt_tokio/mod.rs
@@ -14,7 +14,15 @@ use local_worker::LocalWorker;
 pub(crate) fn get_default_runtime_size() -> usize {
     // We use num_cpus as std::thread::available_parallelism() does not take
     // system resource constraint (e.g.: cgroups) into consideration.
-    num_cpus::get()
+    #[cfg(not(feature = "wasi"))]
+    {
+        num_cpus::get()
+    }
+    // WASI does not support multi-threading at this moment.
+    #[cfg(feature = "wasi")]
+    {
+        1
+    }
 }
 
 #[inline(always)]
